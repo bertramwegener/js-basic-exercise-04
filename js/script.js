4be31d261f7a49c99dc607dc7b7e599a4a137082
bertram wegener <eaa24bkw@students.eaaa.dk>
2025-01-22 14:57:35 +0100

    done

---
+++ js/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const lerning = ("I'm learning to code with JavaScript");
+
+const lerning = ("I want to become a good JavaScript programmer");
+
+console.log(${lerning});
+
+// const kan ikke ændrers og er derfor en konstant, dette skaber en error
